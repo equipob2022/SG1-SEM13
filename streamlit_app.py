9420f6ecaf3541a8c5d198817d45293d3aca1ce2
equipob2022 <119902400+equipob2022@users.noreply.github.com>
2022-12-06 20:21:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,15 @@
+import streamlit as st
+from multiapp import MultiApp
+from apps import home, model, model3 , modelRF  # import your app modules here model2
 
+app = MultiApp()
+
+st.markdown("""
+#  Inteligencia de Negocios - Equipo B
+""")
+
+# Add all your application here
+app.add_app("Home", home.app)
+app.add_app("Modelo LSTM", modelo1.app)
+# The main app
+app.run()
