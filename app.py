fb792200cf77ac760cd9951c82fc5db6735edf43
equipob2022 <119902400+equipob2022@users.noreply.github.com>
2022-12-07 19:59:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = MultiApp()
 
 st.markdown("""
 # Inteligencia de Negocios - Equipo B
-This multi-page app is using the [streamlit-multiapps](https://github.com/upraneelnihar/streamlit-multiapps) framework developed by [Praneel Nihar](https://medium.com/@u.praneel.nihar). Also check out his [Medium article](https://medium.com/@u.praneel.nihar/building-multi-page-web-app-using-streamlit-7a40d55fa5b4).
+
 """)
 # Add all your application here
 app.add_app("Home", home.app)
