ea5a1a5658af2e5f872ccf9a0e9fd6df6e56b804
equipob2022 <119902400+equipob2022@users.noreply.github.com>
2022-12-07 19:11:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ my_variable = "From Main App.py Page"
 def main():
     st.title("Streamlit Multi-Page")
     st.subheader("Main Page")
+    st.write(my_variable)
     
 if __name__ == '__main__':
   main()
