a3c9d9b8770ae6a824267b6a4b787fadbe8591c4
equipob2022 <119902400+equipob2022@users.noreply.github.com>
2022-12-07 20:18:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import home, data, model,description,modelo1 # import your app modules here
+from apps import home, data, model, description, modelo1 # import your app modules here
 
 app = MultiApp()
 
@@ -12,6 +12,6 @@ st.markdown("""
 app.add_app("Description", description.app)
 app.add_app("Data", data.app)
 app.add_app("Model", model.app)
-app.add_app("Modelo 1", modelo1.app)
+app.add_app("Modelo1", modelo1.app)
 # The main app
 app.run()
