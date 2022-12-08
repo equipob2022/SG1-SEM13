73f6cb5751aea201e3df7aef09155194161d0351
equipob2022 <119902400+equipob2022@users.noreply.github.com>
2022-12-08 18:13:11 -0500

    Update model2.py

--- apps/model2.py
+++ apps/model2.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import pandas_datareader as datas
+from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
 
 def app():
@@ -22,3 +23,45 @@ def app():
 
     st.subheader('Datos del 2004 al 2022') 
     st.write(df.describe())
+
+    #Visualizaciones 
+    st.subheader('Closing Price vs Time')
+    fig = plt.figure(figsize = (12,6))
+    plt.plot(df.Close)
+    st.pyplot(fig)
+    
+    # Añadiendo indicadores para el modelo
+    df['Open-Close'] = df.Open - df.Close
+    df['High-Low'] = df.High - df.Low
+    
+    # Modelo SVC
+    
+    ## Variables predictoras
+    X = df[['Open-Close', 'High-Low']]
+    ## Variable objetivo
+    y = np.where(df['Close'].shift(-1) > df['Close'], 1, 0)
+    ## División data de entrenamiento y prueba
+    split_percentage = 0.8
+    split = int(split_percentage*len(df))
+    ## Entrenando el dataset
+    X_train = X[:split]
+    y_train = y[:split]
+    ## Testeando el dataset
+    X_test = X[split:]
+    y_test = y[split:]
+    ## Creación del modelo
+    modelo = LogisticRegression().fit(X_train, y_train)
+    ## Predicción del test
+    y_pred = modelo.predict(X_test)
+    
+    # Señal de predicción 
+    
+    df['Predicted_Signal'] = modelo.predict(X)
+    ## Añadiendo columna condicional
+    conditionlist = [
+    (df['Predicted_Signal'] == 1) ,
+    (df['Predicted_Signal'] == 0)]
+    choicelist = ['Comprar','Vender']
+    df['Decision'] = np.select(conditionlist, choicelist)
+    st.subheader('Predicción de señal de compra o venta') 
+    st.write(df)  
