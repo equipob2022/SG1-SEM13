089a8dde8f139c3f196e073d566c6c52bba851ff
equipob2022 <119902400+equipob2022@users.noreply.github.com>
2022-12-07 19:15:49 -0500

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -2,6 +2,10 @@ import streamlit as st
 
 # Sharing variables among pages
 from app import my_variable
+from modelo1 import my_calc1
 
 st.subheader("Inicio")
 st.write(my_variable)
+
+st.title(my_calc)
+
