5805c907242abbb5c674b5eac3f1edb6aedb6895
Luz Elena Torres Talaverano <luzelena.torres@unmsm.edu.pe>
2022-12-07 19:45:04 -0500

    update new

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 import streamlit as st
+from multiapp import MultiApp
+from apps import home, data, model # import your app modules here
 
-my_variable = "From Main App.py Page"
+app = MultiApp()
 
-def main():
-    st.title("Streamlit Multi-Page")
-    st.subheader("Main Page")
-    st.write(my_variable)
-    
-    submenu = st.sidebar.selectbox("SubMenu",["Inicio","Modelo 1","Modelo 2","Modelo 3","Modelo 4"])
-    if submenu == "Inicio":
-       st.subheader("Inicio")
-    
-    
-if __name__ == '__main__':
-  main()
+st.markdown("""
+# Multi-Page App
+
+This multi-page app is using the [streamlit-multiapps](https://github.com/upraneelnihar/streamlit-multiapps) framework developed by [Praneel Nihar](https://medium.com/@u.praneel.nihar). Also check out his [Medium article](https://medium.com/@u.praneel.nihar/building-multi-page-web-app-using-streamlit-7a40d55fa5b4).
+
+""")
+
+# Add all your application here
+app.add_app("Home", home.app)
+app.add_app("Data", data.app)
+app.add_app("Model", model.app)
+# The main app
+app.run()
--- modelo1.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.subheader("Modelo 1")
-
-my_calc1 = "Esta es la pagina del modelo 1"
