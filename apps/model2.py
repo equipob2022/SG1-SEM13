f5f31a46990e87a6fe24d94a0304ade0c402ea71
equipob2022 <119902400+equipob2022@users.noreply.github.com>
2022-12-08 15:31:35 -0500

    Update model2.py

--- apps/model2.py
+++ apps/model2.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import pandas_datareader as datas
+from sklearn import metrics
 
 def app():
     st.title('Model 2 - Logistic Regression')
@@ -9,3 +11,14 @@ def app():
     #end = '2022-01-20'
     start = st.date_input('Start' , value=pd.to_datetime('2004-08-18'))
     end = st.date_input('End' , value=pd.to_datetime('today'))
+    
+    st.title('Predicción de tendencia de acciones')
+
+    user_input = st.text_input('Introducir cotización bursátil' , 'NTDOY')
+
+    df = datas.DataReader(user_input, 'yahoo', start, end)
+    
+    # Describiendo los datos
+
+    st.subheader('Datos del 2004 al 2022') 
+    st.write(df.describe())
