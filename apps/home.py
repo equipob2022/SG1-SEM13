e701fbacf50f28c233c88c0d5ecf92f789370222
equipob2022 <119902400+equipob2022@users.noreply.github.com>
2022-12-07 22:08:48 -0500

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -3,30 +3,19 @@ import streamlit as st
 def app():
     st.header('Companía Tecnológica Nintendo Co., NTDOY')
 
-    st.subheader('INTEGRANTES: ')
-     
+    st.subheader('INTEGRANTES:')   
+    
     st.write('Alania Inga, Gustavo Adolfo - 19200279')
-    
     st.write('Araujo Santillan, Pedro Angel - 19200066')
-    
     st.write('Benites Narrea, Elvis Saul - 19200068') 
-    
     st.write('Calderon Herrera, Miguel Angel - 19200071')
-
     st.write('Fernandez Caillahua, Felix Alfonso - 19200078')    
-
     st.write('Huarhuachi Ortega, Andrea Mariana - 19200267')
-    
     st.write('Huaman Ampuero, Lucero Marysol - 19200081')
-    
     st.write('Quito Cucho, Giajahira Cristhel - 19200298')
-    
     st.write('Ramos Rivas, Kevin Keyler - 19200096')
-    
-    st.write('Rojas Villanueva, Paula Elianne - 19200266')    
-    
+    st.write('Rojas Villanueva, Paula Elianne - 19200266')     
     st.write('Torres Talaverano, Luz Elena - 19200294')
-    
     st.write('Zenobio Pariasca, Edgar Alonso - 19200245')
     
 
