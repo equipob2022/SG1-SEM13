fdf377eb35ffabf9abdb120716db76b16c4964be
equipob2022 <119902400+equipob2022@users.noreply.github.com>
2022-12-07 19:59:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ from apps import home, data, model # import your app modules here
 app = MultiApp()
 
 st.markdown("""
-#Inteligencia de Negocios - Equipo B
+# Inteligencia de Negocios - Equipo B
+This multi-page app is using the [streamlit-multiapps](https://github.com/upraneelnihar/streamlit-multiapps) framework developed by [Praneel Nihar](https://medium.com/@u.praneel.nihar). Also check out his [Medium article](https://medium.com/@u.praneel.nihar/building-multi-page-web-app-using-streamlit-7a40d55fa5b4).
 """)
-
 # Add all your application here
 app.add_app("Home", home.app)
 app.add_app("Data", data.app)
