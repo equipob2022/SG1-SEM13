1a25a62d50574252ed243141b8dfc098a645158a
equipob2022 <119902400+equipob2022@users.noreply.github.com>
2022-12-07 23:52:08 -0500

    prueba

--- apps/model2.py
+++ apps/model2.py
@@ -2,5 +2,8 @@ import streamlit as st
 
 def app():
     st.title('Model 2 - Logistic Regression')
-
-
+    
+    #start = '2004-08-18'
+    #end = '2022-01-20'
+    start = st.date_input('Start' , value=pd.to_datetime('2004-08-18'))
+    end = st.date_input('End' , value=pd.to_datetime('today'))
