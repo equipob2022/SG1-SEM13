8bc19e64b67e22d9a1d011f72266a5eadc60bf8a
equipob2022 <119902400+equipob2022@users.noreply.github.com>
2022-12-08 19:41:20 -0500

    Update model1.py

--- apps/model1.py
+++ apps/model1.py
@@ -8,7 +8,18 @@ import yfinance as yf
 
 def app():
     st.title('Model 1 - SVR')
-    data = yf.download('NTDOY')
-    descrip=data.describe()
-    st.write(descrip)
+    #start = '2004-08-18'
+    #end = '2022-01-20'
+    start = st.date_input('Start' , value=pd.to_datetime('2004-08-18'))
+    end = st.date_input('End' , value=pd.to_datetime('today'))
+    
+    st.title('Predicción de tendencia de acciones')
 
+    user_input = st.text_input('Introducir cotización bursátil' , 'NTDOY')
+
+    df = datas.DataReader(user_input, 'yahoo', start, end)
+
+    # Describiendo los datos
+
+    st.subheader('Datos del 2004 al 2022') 
+    st.write(df.describe())
